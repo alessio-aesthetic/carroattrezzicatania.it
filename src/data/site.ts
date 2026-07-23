0312eb01acd47007ab1a268de5c58183bd84b4e4
alessio-aesthetic <alessi.ola.rocchia@gmail.com>
2026-07-23 21:32:51 +0200

    Apply distinct Tailwind Plus visual direction

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,5 +1,6 @@
 export const site = {
     "domain":  "carroattrezzicatania.it",
+    "template":  "salient",
     "description":  "Carroattrezzi a Catania e soccorso stradale a Catania: traino, recupero auto, assistenza moto e interventi rapidi in città e provincia.",
     "tel":  "+390950901842",
     "city":  "Catania",
